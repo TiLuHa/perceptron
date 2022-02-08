b8e3b864ba4130fd69b3126947db2661e8aa82dd
Viktor Reichert <stu212792@mail.uni-kiel.de>
2022-02-08 10:57:39 +0100

    folien 2-1 und 2-2 fertig

--- View/js/lib/helpers.js
+++ View/js/lib/helpers.js
@@ -1,207 +1,213 @@
 /**********************************
 
-RANDOM CRAP TO MAKE MY LIFE EASIER
+ RANDOM CRAP TO MAKE MY LIFE EASIER
 
-**********************************/
+ **********************************/
 
 // POLLUTE THIS NAMESPACE
 var _ = {};
-_.clear = function(){
-	var c = _.clear;
-	_ = {};
-	_.clear = c;
+_.clear = function () {
+    var c = _.clear;
+    _ = {};
+    _.clear = c;
 }; // the most hack-y crap ever
 
 // Pi is for unwashed plebians
-Math.TAU = 2*Math.PI;
+Math.TAU = 2 * Math.PI;
 
 // The poor man's jQuery
-var $ = function(query){
-	return document.querySelector(query);
+var $ = function (query) {
+    return document.querySelector(query);
 };
 
 // Add & Remove INSTANTLY
-var _add = function(self){
-	self.slideshow.dom.appendChild(self.dom);
+var _add = function (self) {
+    self.slideshow.dom.appendChild(self.dom);
 };
-var _remove = function(self){
-	self.slideshow.dom.removeChild(self.dom);
+var _remove = function (self) {
+    self.slideshow.dom.removeChild(self.dom);
 };
 
 // Make Label
-var _makeLabel = function(wordID, config){
-	
-	var dom = document.createElement("div");
-	dom.className = "label";
+var _makeLabel = function (wordID, config) {
 
-	dom.innerHTML = Words.get(wordID);
-	config = config || {};
-	
-	_configText(config, dom);
+    var dom = document.createElement("div");
+    dom.className = "label";
 
-	return dom;
+    dom.innerHTML = Words.get(wordID);
+    config = config || {};
+
+    _configText(config, dom);
+
+    return dom;
 
 };
-var _configText = function(config, dom){
-	if(config.x!==undefined) dom.style.left = config.x+"px";
-	if(config.y!==undefined) dom.style.top = config.y+"px";
-	if(config.w!==undefined) dom.style.width = config.w+"px";
-	if(config.h!==undefined) dom.style.height = config.h+"px";
+var _configText = function (config, dom) {
+    if (config.x !== undefined) dom.style.left = config.x + "px";
+    if (config.y !== undefined) dom.style.top = config.y + "px";
+    if (config.w !== undefined) dom.style.width = config.w + "px";
+    if (config.h !== undefined) dom.style.height = config.h + "px";
 
-	if(config.noSelect) dom.classList.add("no-select");
+    if (config.noSelect) dom.classList.add("no-select");
 
-	if(config.rotation!==undefined) dom.style.transform = "rotate("+config.rotation+"deg)";
-	if(config.align!==undefined) dom.style.textAlign = config.align;
-	if(config.color!==undefined) dom.style.color = config.color;
-	if(config.size!==undefined) dom.style.fontSize = config.size;
-	if(config.width!==undefined) dom.style.width = config.width;
-	if(config.lineHeight) dom.style.lineHeight = config.lineHeight+"em";
+    if (config.rotation !== undefined) dom.style.transform = "rotate(" + config.rotation + "deg)";
+    if (config.align !== undefined) dom.style.textAlign = config.align;
+    if (config.color !== undefined) dom.style.color = config.color;
+    if (config.size !== undefined) dom.style.fontSize = config.size;
+    if (config.width !== undefined) dom.style.width = config.width;
+    if (config.lineHeight) dom.style.lineHeight = config.lineHeight + "em";
 }
 
-var _moveX =  function (obj, xRange){
-	obj.dom.style.left = parseInt(obj.dom.style.left.slice(0,-2)) + xRange;
+var _moveX = function (obj, xRange, time = 0) {
+    setTimeout(() => obj.dom.style.left = parseInt(obj.dom.style.left.slice(0, -2)) + xRange,
+        time)
 }
 
-var _moveY =  function (obj, yRange){
-	obj.dom.style.top = parseInt(obj.dom.style.top.slice(0,-2)) + yRange;
+var _moveY = function (obj, yRange, time = 0) {
+    setTimeout(() => obj.dom.style.top = parseInt(obj.dom.style.top.slice(0, -2)) + yRange,
+        time)
 }
 
 // Tween
-var Tween_get = function(target){
-	return Tween.get(target, {useTicks:true});
+var Tween_get = function (target) {
+    return Tween.get(target, {useTicks: true});
 }
-var _s = function(seconds){
-	return Math.ceil(Ticker.framerate*seconds); // converts seconds to ticks
+var _s = function (seconds) {
+    return Math.ceil(Ticker.framerate * seconds); // converts seconds to ticks
 };
 
 // Animation
-var _hide = function(object){
-	object.dom.style.opacity = 0;
+var _hide = function (object) {
+    object.dom.style.opacity = 0;
+    object.dom.classList.add("unclickable")
 };
-var _show = function(object, time = 0){
-	setTimeout(function () {
-		object.dom.style.opacity = 1;
-	}, time);
+var _show = function (object, time = 0) {
+    object.dom.classList.remove("unclickable")
+    setTimeout(function () {
+        object.dom.style.opacity = 1;
+    }, time);
 };
-var _fadeIn = function(object, time){
-	setTimeout(function(){
-		object.dom.classList.add("fader");
-		object.dom.style.opacity = 1;
-		setTimeout(function(){
-			object.dom.classList.remove("fader");
-		},500);
-	},time);
+var _fadeIn = function (object, time) {
+    object.dom.classList.remove("unclickable")
+    setTimeout(function () {
+        object.dom.classList.add("fader");
+        object.dom.style.opacity = 1;
+        setTimeout(function () {
+            object.dom.classList.remove("fader");
+        }, 500);
+    }, time);
 };
 var _fadeOut = function (object, time, opacity = 0) {
-	setTimeout(function(){
-		object.dom.classList.add("fader");
-		object.dom.style.opacity = opacity;
-		setTimeout(function(){
-			object.dom.classList.remove("fader");
-		},500);
-	},time);
+    object.dom.classList.add("unclickable")
+    setTimeout(function () {
+        object.dom.classList.add("fader");
+        object.dom.style.opacity = opacity;
+        setTimeout(function () {
+            object.dom.classList.remove("fader");
+        }, 500);
+    }, time);
 };
 
 
 /*******
 
-Hook listeners to an object,
-and unsubscribe ALL AT ONCE
+ Hook listeners to an object,
+ and unsubscribe ALL AT ONCE
 
-*******/
+ *******/
 
 var _listeners = [];
-var listen = function(object, message, callback){
-	var handler = subscribe(message, callback);
-	_listeners.push({
-		object: object,
-		handler: handler
-	});
+var listen = function (object, message, callback) {
+    var handler = subscribe(message, callback);
+    _listeners.push({
+        object: object,
+        handler: handler
+    });
 };
-var unlisten = function(object){
-	var objectListeners = _listeners.filter(function(l){
-		return l.object==object;
-	});
-	objectListeners.forEach(function(objectListener){
-		unsubscribe(objectListener.handler); // unsubscribe
-		_listeners.splice(_listeners.indexOf(objectListener), 1); // but also FORGET it
-	});
+var unlisten = function (object) {
+    var objectListeners = _listeners.filter(function (l) {
+        return l.object == object;
+    });
+    objectListeners.forEach(function (objectListener) {
+        unsubscribe(objectListener.handler); // unsubscribe
+        _listeners.splice(_listeners.indexOf(objectListener), 1); // but also FORGET it
+    });
 }
 
-var debugListeners = function(){
-	var count = 0;
-	for(var sub in c_){
-		count += c_[sub].length;
-	}
-	console.log("there are currently "+count+" listeners!");
+var debugListeners = function () {
+    var count = 0;
+    for (var sub in c_) {
+        count += c_[sub].length;
+    }
+    console.log("there are currently " + count + " listeners!");
 };
 
 /*******
 
-Make a Sprite. e.g:
+ Make a Sprite. e.g:
 
-_makeSprite("bg", {width:960});
+ _makeSprite("bg", {width:960});
 
-*******/
-function _makeSprite(textureName, options){
-	options = options || {};
+ *******/
+function _makeSprite(textureName, options) {
+    options = options || {};
 
-	// Make Sprite
-	var sprite = new PIXI.Sprite(PIXI.loader.resources[textureName].texture);
+    // Make Sprite
+    var sprite = new PIXI.Sprite(PIXI.loader.resources[textureName].texture);
 
-	// Options
-	if(options.width!==undefined) _scaleToWidth(sprite, options.width);
-	if(options.anchorX!==undefined) sprite.anchor.x=options.anchorX;
-	if(options.anchorY!==undefined) sprite.anchor.y=options.anchorY;
+    // Options
+    if (options.width !== undefined) _scaleToWidth(sprite, options.width);
+    if (options.anchorX !== undefined) sprite.anchor.x = options.anchorX;
+    if (options.anchorY !== undefined) sprite.anchor.y = options.anchorY;
 
-	// Gimme
-	return sprite;
+    // Gimme
+    return sprite;
 }
 
 /*******
 
-Make a MovieClip. e.g:
+ Make a MovieClip. e.g:
 
-_makeSprite("button", {width:960});
+ _makeSprite("button", {width:960});
 
-*******/
-function _makeMovieClip(resourceName, options){
-	options = options || {};
+ *******/
+function _makeMovieClip(resourceName, options) {
+    options = options || {};
 
-	// Make that MovieClip!
-	var resources = PIXI.loader.resources;
-	var resource = resources[resourceName];	
-	if(!resource) throw Error("There's no MovieClip named '"+resourceName+"'!");
-	var numFrames = Object.keys(resource.data.frames).length;
-	var frames = [];
-	for(var i=0; i<numFrames; i++){
-		var str = "0000" + i; // FOUR leading zeroes
-		str = str.substr(str.length-4);
-		frames.push(PIXI.Texture.fromFrame(resourceName+str));
-	}
-	var mc = new PIXI.extras.MovieClip(frames);
+    // Make that MovieClip!
+    var resources = PIXI.loader.resources;
+    var resource = resources[resourceName];
+    if (!resource) throw Error("There's no MovieClip named '" + resourceName + "'!");
+    var numFrames = Object.keys(resource.data.frames).length;
+    var frames = [];
+    for (var i = 0; i < numFrames; i++) {
+        var str = "0000" + i; // FOUR leading zeroes
+        str = str.substr(str.length - 4);
+        frames.push(PIXI.Texture.fromFrame(resourceName + str));
+    }
+    var mc = new PIXI.extras.MovieClip(frames);
 
-	// Options
-	mc.gotoAndStop(0);
-	mc.anchor.x = 0.5;
-	mc.anchor.y = 0.5;
-	if(options.width!==undefined) _scaleToWidth(mc, options.width);
-	if(options.anchorX!==undefined) mc.anchor.x=options.anchorX;
-	if(options.anchorY!==undefined) mc.anchor.y=options.anchorY;
-	if(options.scale!==undefined) mc.scale.x=mc.scale.y=options.scale;
+    // Options
+    mc.gotoAndStop(0);
+    mc.anchor.x = 0.5;
+    mc.anchor.y = 0.5;
+    if (options.width !== undefined) _scaleToWidth(mc, options.width);
+    if (options.anchorX !== undefined) mc.anchor.x = options.anchorX;
+    if (options.anchorY !== undefined) mc.anchor.y = options.anchorY;
+    if (options.scale !== undefined) mc.scale.x = mc.scale.y = options.scale;
 
-	// Gimme
-	return mc;
+    // Gimme
+    return mc;
 
 }
 
 function _shuffleArray(array) {
-	var tmp, current, top = array.length;
-	if(top) while(--top) {
-		current = Math.floor(Math.random() * (top + 1));
-		tmp = array[current];
-		array[current] = array[top];
-		array[top] = tmp;
-	}
-	return array;
+    var tmp, current, top = array.length;
+    if (top) while (--top) {
+        current = Math.floor(Math.random() * (top + 1));
+        tmp = array[current];
+        array[current] = array[top];
+        array[top] = tmp;
+    }
+    return array;
 }
--- View/js/slides/02_Slides_Heatmap.js
+++ View/js/slides/02_Slides_Heatmap.js
@@ -1,9 +1,137 @@
 SLIDES.push(
     {
         id: "blackbox",
-
+        onjump: function (self) {
+            _.slideCounter = "slideCounter";
+            self.add({
+                id: _.slideCounter, type: "TextBox",
+                x: 0, y: 0, width: 50, height: 50,
+                align: "center", color: "#aaa", size: 17,
+                text: ""
+            });
+        },
         onstart: function (self) {
             let o = self.objects;
+            o[_.slideCounter].setText("2-1")
+
+            _.topWords = "topWords";
+            self.add({
+                id: _.topWords, type: "TextBox", text_id: "02_title",
+                x: 130, y: 10, width: 700, height: 100, align: "center"
+            });
+
+            _.tablet = "tablet";
+            self.add({
+                id: _.tablet, type: "ImageBox",
+                src: "assets/birnen/tablet1.jpg",
+                x: 600, y: 170, width: 438 / 3, height: 689 / 3,
+            });
+
+            _.tochter = "tochterBild";
+            self.add({
+                id: _.tochter, type: "ImageBox",
+                src: "assets/Jochen/Tochter.PNG",
+                x: 600, y: 60, width: 380 / 2.1, height: 545 / 2.1,
+            });
+
+            _.jochen = "jochenBild";
+            self.add({
+                id: _.jochen, type: "ImageBox",
+                src: JochenFaces.erstaunt,
+                x: 200, y: 60, width: 380 / 2, height: 545 / 2,
+            });
+
+            _.btmWords = "btmWords";
+            self.add({
+                id: _.btmWords, type: "TextBox", text_id: "02_text1",
+                x: 130, y: 347, width: 700, height: 100, align: "center"
+            })
+
+            _.nextMiddle = "nextMiddle";
+            self.add({
+                id: _.nextMiddle, type: "Button",
+                x: 383, y: 463,
+                text_id: "01_button_next", uppercase: false,
+                onclick: () => publish("slideshow/scratch")
+            });
+
+            _hide(o[_.tablet]);
+            _hide(o[_.jochen]);
+            _hide(o[_.tochter]);
+            _hide(o[_.btmWords]);
+            _hide(o[_.nextMiddle]);
+            _show(o[_.jochen]);
+            _fadeIn(o[_.tochter], 400);
+            _fadeIn(o[_.btmWords], 800)
+            _show(o[_.tablet], 1000)
+            _moveX(o[_.tablet], -200, 1200)
+            _fadeIn(o[_.nextMiddle], 1600)
+        },
+        onend: function (self) {
+            let o = self.objects;
+            self.remove(_.topWords)
+            _hide(o[_.tablet])
+            _hide(o[_.jochen])
+            _hide(o[_.btmWords])
+            _hide(o[_.nextMiddle])
+            _hide(o[_.tochter])
+
+        }
+    },
+    {
+        onstart: function (self) {
+            let o = self.objects;
+            o[_.slideCounter].setText("2-2")
+
+            _.yPosTopBtm = 100;
+            _.yStepToNextBtm = 60;
+            _.fotoBtm = "fotoBtm";
+            let i = 0;
+            self.add({
+                id: _.fotoBtm, type: "Button",
+                size: "long", x: 304, y: _.yPosTopBtm + (i++)*_.yStepToNextBtm,
+                text_id: "02_button_foto_machen", uppercase: false,
+                //active: false,
+                onclick: () => publish("slideshow/scratch")
+            });
+            _.marmeladeBtm = "marmeladeBtm";
+            self.add({
+                id: _.marmeladeBtm, type: "Button",
+                size: "long", x: 304, y: _.yPosTopBtm + (i++)*_.yStepToNextBtm,
+                text_id: "02_button_birnenmarmelade", uppercase: false,
+                active: false
+                //onclick: () => publish("slideshow/next")
+            });
+            _.kuchenBtm = "kuchenBtm";
+            self.add({
+                id: _.kuchenBtm, type: "Button",
+                size: "long", x: 304, y: _.yPosTopBtm + (i++)*_.yStepToNextBtm,
+                text_id: "02_button_birnenkuchen", uppercase: false,
+                active: false,
+                //onclick: () => publish("slideshow/next")
+            });
+            _.bierBtm = "bierBtm";
+            self.add({
+                id: _.bierBtm, type: "Button",
+                size: "long", x: 304, y: _.yPosTopBtm + (i++)*_.yStepToNextBtm,
+                text_id: "02_button_birnenbier", uppercase: false,
+                active: false,
+                //onclick: () => publish("slideshow/next")
+            });
+        },
+        onend: function (self) {
+            let o = self.objects;
+            _hide(o[_.fotoBtm])
+            _hide(o[_.marmeladeBtm])
+            _hide(o[_.kuchenBtm])
+            _hide(o[_.bierBtm])
+        }
+    },
+    {
+        onstart: function (self) {
+            let o = self.objects;
+            o[_.slideCounter].setText("2-3")
+
             _.conection1 = "conection1";
             _.conection2 = "connection2";
             _.weigth2 = "weigth2";
